76b6687190c2dcff028b0f2afe4290cd59642d8f
sakula1996 <akulas1996@gmail.com>
2018-03-26 19:08:49 -0700

    Camera can read QR codes
    * Now camera can read barcode and displays the value of barcode or qrcode on screen

--- app/src/main/java/com/example/sachin/treasure_hunt_android/MainActivity.java
+++ app/src/main/java/com/example/sachin/treasure_hunt_android/MainActivity.java
@@ -1,44 +1,122 @@
 package com.example.sachin.treasure_hunt_android;
 
-import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    ImageView imageView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private SurfaceView cameraView;
+    private TextView barcodeValue;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
 
-        String text="https://www.google.ca/maps/place/Thompson+Rivers+University/@50.672496,-120.370616,15z/data=!4m2!3m1!1s0x0:0x9373b6f9763ebbf4?sa=X&ved=0ahUKEwi8gJfImIvaAhVD6GMKHbQkC3EQ_BIIsAEwEQ" ;// Whatever you need to encode in the QR code
-        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-        try {
-            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
-            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-            imageView.setImageBitmap(bitmap);
-        } catch (WriterException e) {
-            e.printStackTrace();
-        }
+        cameraView = (SurfaceView) findViewById(R.id.surface_view);
+        barcodeValue = (TextView) findViewById(R.id.barcode_value);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1600, 1024)
+                .setAutoFocusEnabled(true) //you should add this feature
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    //noinspection MissingPermission
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor() {
+            @Override
+            public void release() {
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    barcodeValue.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Update barcode value to TextView
+//                            String s = barcodes.valueAt(0).displayValue;
+                             barcodeValue.setText(barcodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+
+
+
     }
 
-    //Barcode will be generated on clicking this button
-    public void generateBarCode(View view){
-        
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraSource.release();
+        barcodeDetector.release();
     }
-
-
 }
+//Barcode will be generated on clicking this button
+//    public void generateBarCode(String linkAstext){
+//        String text=linkAstext ;// Whatever you need to encode in the QR code
+//        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+//        try {
+//            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+//            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+//            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+//            imageView.setImageBitmap(bitmap);
+//        } catch (WriterException e) {
+//            e.printStackTrace();
+//        }
+//    }
