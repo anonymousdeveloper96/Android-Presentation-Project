ac32910ca8e51a3b5aa37f6a2f291e765488db9d
sakula1996 <akulas1996@gmail.com>
2018-03-26 17:00:01 -0700

    Generating Bar code
    Now I can generetarte bar code with the text.
    Works for location, plain text, url links

--- app/src/main/java/com/example/sachin/treasure_hunt_android/MainActivity.java
+++ app/src/main/java/com/example/sachin/treasure_hunt_android/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.sachin.treasure_hunt_android;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+        String text="https://www.google.ca/maps/place/Thompson+Rivers+University/@50.672496,-120.370616,15z/data=!4m2!3m1!1s0x0:0x9373b6f9763ebbf4?sa=X&ved=0ahUKEwi8gJfImIvaAhVD6GMKHbQkC3EQ_BIIsAEwEQ" ;// Whatever you need to encode in the QR code
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
+
+    //Barcode will be generated on clicking this button
+    public void generateBarCode(View view){
+        
+    }
+
+
 }
